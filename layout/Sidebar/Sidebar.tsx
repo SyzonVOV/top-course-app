0346da350737f19b9347b479111f903e8dcac144
SyzonVOV <sizonenko89@gmail.com>
2021-07-17 20:00:46 +0300

    style: Add logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import cn from 'classnames';
 import { SidebarProps } from './Sidebar.props';
-import styles from './Sidebar.module.css';
 import { Menu } from './../Menu';
+import Logo from '../logo.svg';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Search</div>
       <Menu />
     </aside>);
 };
