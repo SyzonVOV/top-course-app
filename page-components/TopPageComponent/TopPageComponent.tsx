b270e032ad2f5451f5e2d5e76857222cb180694b
SyzonVOV <sizonenko89@gmail.com>
2021-07-24 19:48:19 +0300

    fix: recuder in TopPageComponent

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import { sortReducer } from '../../components/Sort/sort.reducer';
 import styles from './TopPageComponent.module.css';
@@ -12,6 +12,10 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
   const setSort = (sort: SortEnum) => {
     dispathSort({ type: sort });
   };
+
+  useEffect(() => {
+    dispathSort({ type: 'reset', initialState: products });
+  }, [products]);
 
   return (
     <div className={styles.wrapper}>
