e4886fbfe67212e1a275e105b569b367820c944c
SyzonVOV <sizonenko89@gmail.com>
2021-07-28 20:02:09 +0300

    feat: Add meta + OpenGraph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>MyTop - наш лучший топ</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+      <meta property="og:locale" content="ru_RU" />
     </Head>
     <Component {...pageProps} />
   </>;
